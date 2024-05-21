ae4287c679075126f8507a9d3322e878c1fcfd28
faqsarg <fsargiottodev@gmail.com>
2024-05-20 21:42:59 -0300

    accept tcp connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpListener;
+
+const IP: &str = "127.0.0.1";
+const PORT: &str = "8080";
+
 fn main() {
-    println!("Hello, world!");
+    let addr = format!("{}:{}", IP, PORT);
+
+    // TODO: (faqsarg - 20/05/2024) handle error properly
+    let lsnr = TcpListener::bind(addr).unwrap();
+
+    for stream in lsnr.incoming() {
+        // TODO: (faqsarg - 20/05/2024) avoid unwrap?
+        let stream = stream.unwrap();
+
+        println!("connection established");
+    }
 }
